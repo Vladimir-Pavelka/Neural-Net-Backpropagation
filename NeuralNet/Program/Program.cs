3934d5e5237d72eb5ec3b8ba2a6f661ffb5dbc35
Vladimir Pavelka <pavelka.vlado@gmail.com>
2019-03-13 17:56:45 +0100

    cleanup

--- NeuralNet/Program/Program.cs
+++ NeuralNet/Program/Program.cs
@@ -1,14 +1,13 @@
 ﻿namespace Program
 {
     using System;
-    using System.Diagnostics;
     using System.Threading;
     using System.Threading.Tasks;
     using System.Windows.Forms;
     using System.Windows.Threading;
     using NeuralNet;
+    using SimpleChart;
     using Training;
-    using Charting;
 
     public class Program
     {
@@ -16,51 +15,16 @@
         public static void Main()
         {
             var net = new NeuralNetwork(2, 2, 1, ActivationFunctions.Sigmoid);
+            var trainingCfg = new TrainingConfiguration { LearningRate = 0.1, MaxEpoch = 512000 };
             var trainingSet = PopularProblems.Xor;
-            var trainCfg = new TrainingConfiguration { LearningRate = 0.1, MaxEpoch = 1024000 };
 
-            //Method1(net, trainingSet, trainCfg);
-            var stopwatch = Stopwatch.StartNew();
-            Method2(net, trainingSet, trainCfg);
-            var elapsedMs = stopwatch.ElapsedMilliseconds;
+            var chartWindow = SimpleChart.LaunchInNewThread();
+            chartWindow.Title = "Learning progress";
+
+            Backpropagation.Train(net, trainingCfg, trainingSet, chartWindow.AddPoint);
 
             Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
-        }
-
-        private static void Method1(NeuralNetwork net, TrainingSet trainSet, TrainingConfiguration cfg)
-        {
-            var chartWindow = new SimpleChart();
-
-            Task.Run(() =>
-                Backpropagation.Train(net, cfg, trainSet, x => chartWindow.AddChartPoint(x))
-            );
-
-            Application.Run(chartWindow);
-        }
-
-        private static void Method2(NeuralNetwork net, TrainingSet trainSet, TrainingConfiguration cfg)
-        {
-            SimpleChart chartWindow = null;
-
-            var chartThread = new Thread(() =>
-            {
-                chartWindow = new SimpleChart();
-                chartWindow.FormClosed += (s, args) => Thread.CurrentThread.Abort();
-                chartWindow.Show();
-                Dispatcher.Run();
-            });
-            chartThread.SetApartmentState(ApartmentState.STA);
-            chartThread.Start();
-            while (chartWindow == null) Thread.Sleep(10);
-
-            var dispatcher = Dispatcher.FromThread(chartThread);
-            //dispatcher.Invoke(() => chartWindow.MinValueAxisY = 0.1);
-            //dispatcher.Invoke(() => chartWindow.PointCountAxisX = 300);
-            //dispatcher.Invoke(() => chartWindow.Text = "Yogo");
-            
-            Backpropagation.Train(net, cfg, trainSet,
-                x => chartWindow.AddChartPoint(x));
         }
     }
 }
--- NeuralNet/Tests/BackpropagationTests.cs
+++ NeuralNet/Tests/BackpropagationTests.cs
@@ -8,6 +8,7 @@
 
     public class BackpropagationTests
     {
+        private TrainingConfiguration _trainingCfg;
         private const double Precision = 0.000000001;
 
         [Fact]
@@ -15,9 +16,10 @@
         {
             var net = new NeuralNetwork(2, 2, 1, ActivationFunctions.Sigmoid);
             var trainingSet = PopularProblems.Xor;
+            var trainingCfg = new TrainingConfiguration { LearningRate = 0.1, MaxEpoch = 10000 };
 
             var trainingErrors = new List<double>();
-            Backpropagation.Train(net, new TrainingConfiguration(0.1, 10000, 0), trainingSet, trainingErrors.Add);
+            Backpropagation.Train(net, trainingCfg, trainingSet, trainingErrors.Add);
 
             trainingErrors.Should().BeInDescendingOrder();
         }
@@ -29,8 +31,9 @@
         {
             var net = NeuralNetTestSetup.GetConfiguredNet();
             var trainingSet = new TrainingSet(new[] { new TrainingSample(new[] { 0.05, 0.10 }, new[] { 0.01, 0.99 }) });
+            _trainingCfg = new TrainingConfiguration { LearningRate = 0.5, MaxEpoch = iterationsCount};
 
-            Backpropagation.Train(net, new TrainingConfiguration(0.5, iterationsCount, 0), trainingSet, _ => { });
+            Backpropagation.Train(net, _trainingCfg, trainingSet, _ => { });
             var actualError = ErrorFunctions.TotalError(net, trainingSet);
 
             actualError.Should().BeApproximately(expectedError, Precision);
